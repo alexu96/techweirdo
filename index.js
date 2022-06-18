0399237787cbe164985034d0c64a213d80c20c5a
Gulsan <74457215+alexu96@users.noreply.github.com>
2022-06-18 20:00:38 +0530

    fix

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const Sequelize = require('sequelize');
-const { Op } = require("sequelize");
 const express = require('express');
 const bodyParser = require('body-parser');
 const { STRING } = require('sequelize');
